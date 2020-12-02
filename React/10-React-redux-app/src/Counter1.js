5fe9e9c4e2eff33aa142b1e92817b8b2cee76df6
nrcool <nrcoolove@yahoo.com>
2020-12-02 10:47:44 +0100

    redux store

--- React/10-React-redux-app/src/Counter1.js
+++ React/10-React-redux-app/src/Counter1.js
@@ -4,10 +4,10 @@ import {useSelector, useDispatch} from "react-redux"
 
  export default function Counter1() {
      const dispatch = useDispatch()
-     const count = useSelector((state)=>state.count)
+     const state = useSelector(state=>state)
     return (
         <div>
-            <h2>Counter : {count}  </h2>
+            <h2>Counter : {state.count}  </h2>
             <button onClick={()=>dispatch({type:"increment"})}>increment</button>
         </div>
     )
