47759b7aa93a04ed7684b189a287e60a4a4491c4
nrcool <nrcoolove@yahoo.com>
2020-12-14 10:24:30 +0100

    node modules import and export

---
+++ NodeJs/01-NodeJS-Modules/calculation.js
@@ -0,0 +1,13 @@
+const sum=(a,b)=>{
+    return (a+b)
+}
+
+const diff=(a,b)=>{
+    return (b-a)
+}
+
+/* module.exports.sum= sum;
+module.exports.diff=diff; */
+
+module.exports= {sum,diff}
+
---
+++ NodeJs/01-NodeJS-Modules/main.js
@@ -0,0 +1,6 @@
+const {sum,diff} = require("./calculation")
+
+console.log(sum(2,3))
+console.log(diff(4,2))
+
+/* modules(only javascripts) / packages(javascript and package.json and register it npm)  */
