32fa8d3f830634f5d015ecd2f507faabad4bbc84
nrcool <nrcoolove@yahoo.com>
2021-01-14 15:56:02 +0100

    implementation  of logout and created protected profile route

--- NodeJs/08-express-view-engine/app.js
+++ NodeJs/08-express-view-engine/app.js
@@ -4,6 +4,7 @@ const express = require("express")
 const app = express()
 //express.json middleware
 app.use(express.json())
+//parsing formdata getting directly from form
 app.use(express.urlencoded({extended:true}))
 
 //we are setting view engine for our server
@@ -11,12 +12,12 @@ app.set("view engine","ejs")
 /* app.set("views","./views") */
 
 
-let user = "Ali";
+let user = "";
 let isLogin=false;
 
 app.get("/",(req,res)=>{
     //render is compiling your template file into plain html file
-    res.render("index", {name:user})
+    res.render("index")
 })
 app.get("/login",(req,res)=>{
     res.render("login")
@@ -24,6 +25,30 @@ app.get("/login",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     console.log(req.body)
+    const {username,password}= req.body
+
+    if(username==="ali"&&password==="123"){
+        user=username
+        isLogin=true
+       res.redirect("/profile")
+    }else{
+        res.redirect("/")
+    }
 })
+
+app.get("/profile",(req,res)=>{
+    if(isLogin){
+      res.render("profile",{name:user})  
+    }else{
+        res.redirect("/login")
+    }
+    
+})
+app.get("/logout",(req,res)=>{
+    isLogin=false;
+    user=""
+    res.redirect("/")
+})
+
 
 app.listen(3000, ()=>console.log("server is running on port 3000"))
