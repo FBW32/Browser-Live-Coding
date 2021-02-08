6c473eb4ac7556e1208991cd52178e79a7474ab2
nrcool <nrcoolove@yahoo.com>
2021-02-08 12:15:56 +0100

    task 5 recap

--- NodeJs/09-Record-Shop-Live-Project/controllers/usersController.js
+++ NodeJs/09-Record-Shop-Live-Project/controllers/usersController.js
@@ -1,69 +1,71 @@
+const UserData = require("../model/userModel");
+exports.getAllUsers = async (req, res, next) => {
+  //getting all users from mongoDB
+  try {
+    let allUsers = await UserData.find();
+    res.status(200).send({ allUsers });
+  } catch (err) {
+    next(err);
+  }
+};
 
-const UserData= require("../model/userModel")
-exports.getAllUsers= async(req,res,next)=>{
-    //getting all users from mongoDB
-    try {
-        let allUsers = await UserData.find();
-        res.status(200).send({ allUsers });
-      } catch (err) {
-        next(err);
-      }
-}
+exports.postAddNewUser = async (req, res, next) => {
+  /*    console.log(req.body) */
+  //adding new User into mongoDB
 
-exports.postAddNewUser= async (req,res,next)=>{
- /*    console.log(req.body) */
-    //adding new User into mongoDB
-    try {
-        const user = new UserData(req.body);
-        await user.save(); //store data into database
-        res.status(200).send({ user });
-      } catch (err) {
-        console.log(err.message);
-        /*  res.status(404).send({err:err.message}) */
-        next(err);
-      }
-}
+  try {
+    const user = new UserData(req.body);
+    await user.save(); //store data into database
+    res.status(200).send({ user });
+  } catch (err) {
+    console.log(err.message);
+    /*  res.status(404).send({err:err.message}) */
+    next(err);
+  }
+};
 
-exports.putUpdateUser=async (req,res,next)=>{
-    const {id} = req.params
-    //finding existing user with that id in database and update
-    try {
-        const updatedUser = await UserData.findByIdAndUpdate(id, req.body,{new:true})
-        res.status(200).send({updatedUser})
-    } catch (err) {
-      next(err);
+exports.putUpdateUser = async (req, res, next) => {
+  const { id } = req.params;
+  //finding existing user with that id in database and update
+  try {
+    const updatedUser = await UserData.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).send({ updatedUser });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteSingleUser = async (req, res, next) => {
+  const { id } = req.params;
+  //finding that User //deleting User
+  try {
+    const UserDeleted = await UserData.findByIdAndRemove(id);
+    if (UserDeleted) {
+      res.status(200).send({ UserDeleted });
+    } else {
+      res.status(404).send("Already Deleted that record");
     }
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
-exports.deleteSingleUser=async (req,res,next)=>{
-    const {id} = req.params
-    //finding that User //deleting User
-    try{
-        const UserDeleted= await UserData.findByIdAndRemove(id)
-        if(UserDeleted){
-             res.status(200).send({UserDeleted})
-        }else{
-            res.status(404).send("Already Deleted that record")  
-        }
+exports.getSingleUser = async (req, res, next) => {
+  const { id } = req.params;
+  //get/read single user from mongoDB
+  try {
+    const user = await UserData.findById(id).select("-_id -__v");
+    if (user) {
+      res.status(200).send({ user });
+    } else {
+      res.status(404).send("No such record found with that Id");
     }
-    catch(err){
-        next(err)
-    }
-}
-
-exports.getSingleUser=async (req,res,next)=>{
-    const {id} = req.params
-    //get/read single user from mongoDB
-    try{
-        const user = await UserData.findById(id).select("-_id -__v")
-        if(user){
-            res.status(200).send({user})
-        }
-        else{
-            res.status(404).send("No such record found with that Id")
-        }
-    }catch(err){next(err)}
-}
+  } catch (err) {
+    next(err);
+  }
+};
 
 //CRUD operation
 //RESTful API  representational state transfer
--- NodeJs/09-Record-Shop-Live-Project/routes/usersRoute.js
+++ NodeJs/09-Record-Shop-Live-Project/routes/usersRoute.js
@@ -1,26 +1,37 @@
-const express = require("express")
+const express = require("express");
 
-const router = express.Router()
+const router = express.Router();
 
-const {getAllUsers,postAddNewUser,putUpdateUser,deleteSingleUser,getSingleUser} = require("../controllers/usersController")
-const Validation = require("../middlewares/validationRules")
-const userRules= require("../lib/userRules")
+const {
+  getAllUsers,
+  postAddNewUser,
+  putUpdateUser,
+  deleteSingleUser,
+  getSingleUser,
+} = require("../controllers/usersController");
+
+const Rules = require("../lib/validationRules")
+
+const Validation  =require("../middlewares/validation")
 
 /* GET ALL UserS FROM RESOURCES */
-router.get("/", getAllUsers )
+router.get("/", getAllUsers);
 
 /* POST REQUEST TO ADD NEW User */
-router.post("/",Validation(userRules) , postAddNewUser)
+router.post(
+  "/",
+   Validation(Rules),
+  postAddNewUser
+);
 
 /* PUT REQUEST TO UPDATE SIGNLE User IN DATABASE */
-router.put("/:id",putUpdateUser )
+router.put("/:id", putUpdateUser);
 
 /* DELETE REQUEST TO DELETE SIGNLE User IN DATABASE */
-router.delete("/:id",deleteSingleUser )
+router.delete("/:id", deleteSingleUser);
 
 //GET SIGNLE User FRO  DATABASE
-router.get("/:id",getSingleUser)
-
+router.get("/:id", getSingleUser);
 
 /* DEFAULT EXPORT */
-module.exports = router
+module.exports = router;
