b058248ba1a5eb3067a3d41c309110dd7830fdab
nrcool <nrcoolove@yahoo.com>
2021-02-09 10:58:04 +0100

    added address schema

--- NodeJs/09-Record-Shop-Live-Project/routes/recordsRoute.js
+++ NodeJs/09-Record-Shop-Live-Project/routes/recordsRoute.js
@@ -3,12 +3,14 @@ const express = require("express")
 const router = express.Router()
 
 const {getAllRecords,postAddNewRecord,putUpdateRecord,deleteSingleRecord,getSingleRecord} = require("../controllers/recordsController")
+const recordsRules= require("../lib/validationRules4records")
+const Validation = require("../middlewares/validation")
 
 /* GET ALL RECORDS FROM RESOURCES */
 router.get("/", getAllRecords )
 
 /* POST REQUEST TO ADD NEW RECORD */
-router.post("/", postAddNewRecord)
+router.post("/", Validation(recordsRules), postAddNewRecord)
 
 /* PUT REQUEST TO UPDATE SIGNLE RECORD IN DATABASE */
 router.put("/:id",putUpdateRecord )
